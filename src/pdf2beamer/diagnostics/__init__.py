fc1891dc3e8d031d21628831686ccfb16f69c7f6
LelioG <leliog63@gmail.com>
2026-05-28 15:30:10 +0200

    Delete src/pdf2beamer directory

--- src/pdf2beamer/diagnostics/__init__.py
+++
@@ -1,5 +0,0 @@
-"""PDF diagnostics modules."""
-
-from pdf2beamer.diagnostics.pdf_diagnostics import PDFDiagnostics, PdfDiagnostics, diagnose_pdf
-
-__all__ = ["PDFDiagnostics", "PdfDiagnostics", "diagnose_pdf"]
